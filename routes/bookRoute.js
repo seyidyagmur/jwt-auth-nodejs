60ce76c2e34dc7debb6133cc4e6aaea770a781f2
seyid yagmur <seyid.yagmur@gmail.com>
2019-03-29 03:48:29 +0300

    bookRoute was updated

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -2,17 +2,46 @@ const Boom = require('boom');
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const Book=require('../models/Book');
 const authorizationCheck = require('../services/authFunction').authorizationCheck;
  
 	router.get('/{id}',async (req,res)=>{
+      const _id = req.params.id;
+      Book.findOne({_id}).select('-__v')
+      	   .exec((err, data) => {
+	          if (err) {
+	            res.json(Boom.badRequest(err));
+	            return;
+	          }
+	          if (!data) {
+	            res.json(Boom.notFound('Instructor not found!'));
+	            return;
+	          }
+	          res.json(data);
+	        });
 
 	});
 
 	router.get('/',(req,res)=>{
-	    res.status(200).send({ access_token: req.decoded});
+		Book.find().select('-__v')
+			.exec((err,data)=>{
+				if(err)
+				return res.json(Boom.badRequest(err));
+				if(!data.length)
+        	   	return res.json(Boom.notFound('No book found!'));
+        	 	res.json(data);
+			});
 	});
 	router.post('',authorizationCheck("admin"),async(req,res)=>{
+		let book=new Book(req.body);
+		book.save((err,data)=>{
+				if(err){
+				 res.json(Boom.badRequest(err));
+				 return
+				}
+    	    res.json({ message: 'Book created!', data }).code(201);
 
+		})
 	});
 
 module.exports = router;
